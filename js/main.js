5546d83cbda2cc678e17a4cc3a67f4bd726cb77a
Edgard Morales <edgard@ciencias.unam.mx>
2015-05-25 01:34:39 -0500

    footer busqueda / centrado del paginado

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 $(function() {
 	$('select').material_select();
 	$(".button-collapse").sideNav({'edge': 'left'});
-	height = $(window).height()-114;
+	height = $(window).height()-134;
 	if($('main').outerHeight() < height) {
 		$('main').css('minHeight',height);
 	} else {
 		$('main').css('paddingBottom','55px');
 	}
+	width = $('.pagination li').length * 50;
+	$('pagination').css('width', width+'px');
 });
